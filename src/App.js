4ff85d94ba86ff593ab51b42cd91060839f0941e
Nuno Durão <nunofdurao@gmail.com>
2023-05-25 22:10:17 +0100

    lab Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
+/* eslint-disable no-unused-vars */
 import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import axios from 'axios';
+import CountriesList from './Components/CoutriesList';
+import CountryDetails from './Components/CountryDetails';
+import Header from './Components/Header';
+
+const apiURL = "https://ih-countries-api.herokuapp.com/countries";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const getCountries = async () => {
+    try {
+      let response = await axios.get(apiURL);
+      setCountries(response.data);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getCountries();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +44,31 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Header />
+
+      <div className="container">
+        <div className="row">
+          <CountriesList countries={countries} />
+          {/* React-Router Route rendering the CountryDetails should go here */}
+        </div>
+      </div>
+
+      <Routes>
+        <Route
+          path="/countriesList/:countryId"
+          element={
+            loading ? (
+              <p>It's loading</p>
+            ) : (
+              <CountryDetails countries={countries} />
+            )
+          }
+        />
+      </Routes>
     </div>
   );
 }
 
 export default App;
+
